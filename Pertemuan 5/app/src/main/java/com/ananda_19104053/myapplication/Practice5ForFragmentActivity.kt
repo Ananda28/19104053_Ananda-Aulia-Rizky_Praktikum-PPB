1cec5ed5bf92502f4eba30155ff14316759b19ad
Ananda Aulia <72482619+Ananda28@users.noreply.github.com>
2022-01-23 22:03:03 +0700

    Add files via upload

---
+++ Pertemuan 5/app/src/main/java/com/ananda_19104053/myapplication/Practice5ForFragmentActivity.kt
@@ -0,0 +1,28 @@
+package com.ananda_19104053.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ananda_19104053.myapplication.databinding.ActivityPractice5ForFragmentBinding
+
+class Practice5ForFragmentActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityPractice5ForFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPractice5ForFragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mFragmentManager = supportFragmentManager
+        val mFirstFragment = Practice5FirstFragment()
+        val fragment = mFragmentManager.findFragmentByTag(Practice5FirstFragment::class.java.simpleName)
+
+        if (fragment !is Practice5FirstFragment){
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, mFirstFragment, Practice5FirstFragment::class.java.simpleName)
+                .commit()
+        }
+
+    }
+
+}
