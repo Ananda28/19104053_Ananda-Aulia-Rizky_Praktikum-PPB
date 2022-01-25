de81d3fb7ffacb5ef1443215fa53df053c6dd261
Ananda Aulia <72482619+Ananda28@users.noreply.github.com>
2022-01-26 00:37:36 +0700

    Add files via upload

---
+++ Pertemuan 10/app/src/main/java/com/ananda_19104053/myapplication/Quote.kt
@@ -0,0 +1,13 @@
+package com.ananda_19104053.myapplication
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
